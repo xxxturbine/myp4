0ccb06563c297b230261b8339dae7911ea20a48f
xxxturbine <93062259+xxxturbine@users.noreply.github.com>
2025-06-01 09:47:55 +0800

    Create chage.sh

---
+++ chage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#更新apt源为清华源
+echo "[Info] 正在备份默认apt源..."
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "[Info] 正在替换apt源为清华apt源..."
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse > /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse >> /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse >> /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse >> /etc/apt/sources.list
+echo "[Info] 正在更新源..."
+apt update
+echo "[Info] 正在更新软件..."
+apt upgrade -y
